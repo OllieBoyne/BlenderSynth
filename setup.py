9d8e8c5e55975bc89342881fc574ad75a35b3a13
OllieBoyne <ollieboyne@gmail.com>
2023-06-13 12:43:33 +0100

    Setup fixes

--- setup.py
+++ setup.py
@@ -16,17 +16,29 @@ if "--blender_path" in sys.argv:
 blender_path = get_blender_path(_blender_path)
 python_path = find_blender_python(blender_path) # blender's python path
 
-# install necessary packages to blender's python
-packages = ['imageio[pyav]', 'numpy', 'appdirs']
-subprocess.call([python_path, "-m", "ensurepip"])
-# subprocess.call([python_path, "-m", "pip", "install", "--upgrade", "pip"])
-for package in packages:
-	subprocess.call([python_path, "-m", "pip", "install", package])
+if '--running_from_blender' not in sys.argv:
+	# install necessary packages to blender's python - including blendersynth
+	packages = ['imageio[pyav]', 'numpy', 'appdirs', 'tqdm']
+	subprocess.call([python_path, "-m", "ensurepip"])
+	# subprocess.call([python_path, "-m", "pip", "install", "--upgrade", "pip"])
+	for package in packages:
+		subprocess.call([python_path, "-m", "pip", "install", package])
+
+	# install blendersynth
+	print("----")
+	cmd = [python_path, "setup.py", "install", "--blender_path", blender_path, "--running_from_blender"]
+	subprocess.call(cmd)
+	print("----")
+
+
+else:
+	# remove --running_from_blender from sys.argv
+	sys.argv.remove('--running_from_blender')
 
 # now can set-up from blendersynth directory
 
 setup(
 	name='blendersynth',
 	version='0.1.0',
-	packages=find_packages(include=['blendersynth'])
+	packages=find_packages(include=['blendersynth', 'blendersynth.*']),
 )
