96e06708cac78dff64b4a1d0e0faf32d1be41cb5
GitHub Action <action@github.com>
2023-07-10 22:49:46 +0000

    Update documentation

---
+++ _static/python/examples.capturing_pose.py
@@ -0,0 +1,37 @@
+"""Script to show how to capture the pose of 3D objects."""
+import blendersynth as bsyn
+bsyn.run_this_script(debug = False)
+
+bsyn.load_blend('../resources/scenes/capturing_pose.blend')  # load from scene
+
+comp = bsyn.Compositor()  # create compositor
+
+monkey = bsyn.Mesh.from_scene('Monkey')
+cube = bsyn.Mesh.from_scene('Cube')
+
+bounding_box_visual = comp.get_bounding_box_visual()
+keypoints_visual = comp.get_keypoints_visual(marker='x')  # Create a visual of keypoints
+axis_visual = comp.get_axes_visual()
+
+visual = comp.stack_visuals(bounding_box_visual, keypoints_visual, axis_visual)
+
+bsyn.render.set_resolution(512, 512)
+bsyn.render.set_cycles_samples(10)
+
+comp.define_output('Image', directory='pose')
+comp.define_output(visual, file_name='visual', directory='pose')
+
+# calculate bounding boxes
+objects = [monkey, cube]
+bounding_boxes = bsyn.annotations.bounding_boxes(objects)
+
+# calculate object centres as keypoints
+keypoints = bsyn.annotations.keypoints.project_keypoints([obj.centroid() for obj in objects])
+
+# calculate axes in 2D. In 3D, use e.g. monkey.axes
+axes = bsyn.annotations.get_multiple_axes(objects)
+
+overlay_kwargs = dict(BBox=bounding_boxes, Keypoints=keypoints,
+					  Axes=axes)
+
+comp.render(overlay_kwargs=overlay_kwargs)
