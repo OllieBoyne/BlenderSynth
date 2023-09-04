37a1d14ed5941d1e1fb765a7138394f07f2b451b
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2023-09-04 12:51:03 +0100

    Updated readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
 	name='blendersynth',
-	version='0.0.3',
+	version='0.0.5',
 	install_requires=["appdirs>=1.4.4", "numpy", "tqdm", "ffmpeg-python"],
 	packages=find_packages(include=['blendersynth', 'blendersynth.*']),
 )
