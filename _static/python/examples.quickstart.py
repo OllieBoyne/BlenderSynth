c9a9c69c5c475c7d36340506de324ff93463dde8
GitHub Action <action@github.com>
2023-11-20 13:16:39 +0000

    Update documentation

--- _static/python/examples.quickstart.py
+++ _static/python/examples.quickstart.py
@@ -7,8 +7,12 @@ bsyn.run_this_script(open_blender=False)  # If called from Python, this will run
 comp = bsyn.Compositor()  # Create a new compositor - this manages all the render layers
 
 # Generate some basic scene components. Alternatively, use bsyn.load_blend to load a .blend file
-monkey = bsyn.Mesh.from_primitive('monkey', scale=2, rotation_euler=(0, 0, -0.5))  # Create a new mesh from a primitive
+monkey = bsyn.Mesh.from_primitive('monkey', scale=2, rotation_euler=(0, 0, 0.5))  # Create a new mesh from a primitive
 light = bsyn.Light.create('POINT', location=(0, -5, 0), intensity=100)  # Create a new light
+
+# add a basic material
+monkey.material = bsyn.Material()
+monkey.material.set_bdsf_property("Base Color", (0, 0.8, 0.2, 1))
 
 # Set some render settings
 bsyn.render.set_cycles_samples(10)
