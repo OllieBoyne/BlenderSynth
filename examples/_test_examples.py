2bd651332f8b6a47158470126b28669e60e04754
OllieBoyne <ollieboyne@gmail.com>
2023-11-28 23:24:11 +0000

    More robust _test_examples.py

--- examples/_test_examples.py
+++ examples/_test_examples.py
@@ -2,16 +2,20 @@
 
 import subprocess
 import os
+import sys
 
 example_scripts = [f for f in os.listdir() if f.endswith('.py') and not f.startswith('_')]
+# place animation.py last because it takes a long time to run
+example_scripts = sorted(example_scripts, key=lambda x: x == 'animation.py')
 
 for script in example_scripts:
-	result = subprocess.run(['python', script], capture_output=True, text=True)
+	print(f"Running script {script}")
+	result = subprocess.run([sys.executable, script], capture_output=True, text=True)
 
 	# scan stdout for errors
-	lines = result.stderr.split('\n') +  result.stdout.split('\n')
+	lines = result.stderr.split('\n') + result.stdout.split('\n')
 	for i, line in enumerate(lines):
-		if 'Error' in line:
+		if 'Error' in line or 'Err' in line:
 			print("\n".join([f"✖ Error in script {script}:", *lines[i:i+3], '']))
 			break
 
