a0b3100cca09fff2dbbe36f3a9c39f5b88a9240a
OllieBoyne <ollieboyne@gmail.com>
2023-12-16 13:41:54 +0000

    Version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
 	name='blendersynth',
-	version='0.2.0',
+	version='0.2.2',
 	install_requires=["platformdirs>=3.11.0", "numpy", "tqdm", "ffmpeg-python"],
 	packages=find_packages(include=['blendersynth', 'blendersynth.*']),
 )
